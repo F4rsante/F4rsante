034ccf1c79a60094876ba724faa4708df34c06f7
F4rsante <99933418+F4rsante@users.noreply.github.com>
2022-03-09 11:42:58 -0600

    Update contarCartas.js

--- contarCartas.js
+++ contarCartas.js
@@ -1,3 +1,18 @@
+/* 
+Descripcion:
+En el juego de casino Blackjack el jugador puede sacarle ventaja a la casa llevando el registro del numero relativo de cartas altas y bajas que quedan en la baraja
+Esto se llama "conteo de cartas"
+Tener mas cartas altas en la baraja es una ventaja para el jugador. 
+Se le asigna un valor a cada carta de acuerdo a la siguiente tabla.
+-Cuando el conteo es positivo, el jugador deberia apostar.
+-Cuando el conteo es 0 o negativo, el jugador deberia apostar bajo.
+Cambio de conteo        cartas
+-------------------------------------------------------
++1                      2,3,4,5,6
+0                       7,8,9
+-1                      10,'J','Q','K','A' 
+*/
+
 var conteo=0;
 var decision;
 function contarCartas (carta) {
